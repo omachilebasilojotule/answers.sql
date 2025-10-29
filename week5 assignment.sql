7908cf5d8574493dd0b3dc7f5a15cc137576ed07
OMACHILE BASIL OJOTULE <omachilebasil@gmail.com>
2025-10-29 11:36:19 -0700

    Add files via upload

---
+++ week5 assignment.sql
@@ -0,0 +1,6 @@
+use salesdb
+DROP INDEX IdxPhone ON customers;
+CREATE USER 'bob'@'localhost' IDENTIFIED BY 'Scu3r3!';
+GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P55!23';
+
